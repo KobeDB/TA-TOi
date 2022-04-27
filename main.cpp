0ddaf0167f37aa51727e1bb681c9f63048263784
Kobe De Broeck <kobe.debroeck1@gmail.com>
2022-04-27 18:58:10 +0200

    ENFA can now be converted to .dot graphviz file

--- main.cpp
+++ main.cpp
@@ -11,11 +11,42 @@ using namespace std;
 
 using json = nlohmann::json;
 
+void jsonToDot(ostream& os, json faDesc)
+{
+    os << "digraph finite_state_machine {";
+    os << "rankdir=LR;";
+
+    os << "node [shape = doublecircle];";
+    vector<json> states = faDesc["states"];
+    string startState;
+    for(const auto& s : states) {
+        if(s["starting"])
+            startState = string{s["name"]};
+        if(s["accepting"])
+            os << string{s["name"]} << ";";
+    }
+
+    os << "node [shape = point ]; _qi ;";
+
+    os << "node [shape = circle];";
+    vector<json> transitions = faDesc["transitions"];
+    os << "_qi -> " << startState << ";";
+    for(const auto& t : transitions) {
+        os << string{t["from"]} << " -> " << string{t["to"]} << "[label = \"" << string{t["input"]} <<"\"] ;";
+    }
+
+    os << "}";
+}
+
 int main(){
-    RE re{"+ab", 'e'};
+    RE re{".*+01.10", 'e'};
     ENFA enfa = re.toENFA();
     json enfaDesc = enfa.toJson();
     cout << setw(4) << enfaDesc;
+
+    ofstream ofs{"enfa.dot"};
+    jsonToDot(ofs, enfaDesc);
+
     cout << "Hello there\n";
     return 0;
 }
